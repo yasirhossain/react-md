65de079b3988dfbf6e0bcfddd8a3a3fc0b2b049e
Yasir <yhossain@gmail.com>
2022-08-06 11:28:30 -0700

    added ability to crop image

--- seismic-web/src/pages/AdBuilder.js
+++ seismic-web/src/pages/AdBuilder.js
@@ -4,19 +4,60 @@ import '../styles/AdBuilder.scss';
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 
-function AdBuilder() {
-  const cropperRef = useRef < HTMLImageElement > null;
+const defaultSrc =
+  'https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg';
 
-  const onCrop = () => {
-    const imageElement: any = cropperRef?.current;
-    const cropper: any = imageElement?.cropper;
-    console.log(cropper.getCroppedCanvas().toDataURL());
+function AdBuilder() {
+  const [image, setImage] = useState(defaultSrc);
+  const [cropData, setCropData] = useState('#');
+  const [cropper, setCropper] = useState(null);
+  const onChange = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
+  const getCropData = () => {
+    if (typeof cropper !== 'undefined') {
+      setCropData(cropper.getCroppedCanvas().toDataURL());
+    }
   };
 
   return (
     <div className="ad-builder">
       <h1>Create a Video Slate</h1>
+
+      {/* Step 1: Drag n Drop Initial Image  */}
       <div className="drag-n-drop"></div>
+
+      {/* Step 2: Crop Image Provided  */}
+      <Cropper
+        style={{ height: 400, width: '100%' }}
+        zoomTo={0.5}
+        initialAspectRatio={1}
+        preview=".img-preview"
+        src={image}
+        viewMode={1}
+        minCropBoxHeight={10}
+        minCropBoxWidth={10}
+        background={false}
+        responsive={true}
+        autoCropArea={1}
+        checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+        onInitialized={(instance) => {
+          setCropper(instance);
+        }}
+        guides={true}
+      />
     </div>
   );
 }
